06db91027ded6d7147c3b81ae9bb2f88e2105d28
Boitos Roxana <roxana.boitos@trakosoft.ro>
2023-12-19 20:08:35 +0200

    create ToDoItemModel

---
+++ ToDoList/Models/ToDoItemModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ToDoList.Models
+{
+    public class ToDoItemModel
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int Status { get; set; }
+    }
+}
