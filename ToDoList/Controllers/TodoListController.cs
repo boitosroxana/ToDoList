67d7a69d6053585f84d37570ab206b02fad1afca
Boitos Roxana <roxana.boitos@trakosoft.ro>
2023-12-19 20:25:27 +0200

    create controller

---
+++ ToDoList/Controllers/TodoListController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoList.Controllers
+{
+    public class TodoListController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Edit(int id)
+        {
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            return RedirectToAction("Index");
+        }
+
+
+
+    }
+}
--- ToDoList/Models/ToDoItemModel.cs
+++ ToDoList/Models/ToDoItemModel.cs
@@ -1,9 +1,13 @@
-﻿namespace ToDoList.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Models
 {
     public class ToDoItemModel
     {
         public int Id { get; set; }
 
+
+        [MaxLength(200)]
         public string? Name { get; set; }
 
         public DateTime Date { get; set; }
